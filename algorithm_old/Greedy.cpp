115d20c4ea2e69c6afa0ac1e165b01a0ebeac3bc
KyNgo09 <kydaingo@gmail.com>
2024-10-27 12:07:17 +0700

    Clean code for greedy

--- algorithm_old/Greedy.cpp
+++ algorithm_old/Greedy.cpp
@@ -7,245 +7,235 @@
 
 using namespace std;
 
-// Khai báo kiểu cấu trúc Edge
-// - Điểm đầu và cuối của cung
-// - Trọng số cung: chi phí của đường đi
-typedef struct {
-	char u, v;
-	int g;
+//Khai báo cấu trúc Edge
+typedef struct{
+    char u, v; //Điểm đầu và điểm cuối của cung
+    int g; //Trong số cung (chi phí của đường đi)
 } Edge;
 
-// Khai báo kiểu cấu trúc Map
-// - Danh sách các điểm
-// - Danh sách hueristic của từng điểm
-// - Danh sách đường đi
-typedef struct {
-	vector<char> point_list;
-	vector<int> h_list;
-	vector<Edge> edge_list;
-} Map;
+//Khai báo cấu trúc bản đồ (Map)
+typedef struct{
+    vector<char> point_list; //Danh sách các điểm
+    vector<int> h_list; //Danh sách heuristic của từng điểm
+    vector<Edge> edge_list; //Danh sách các đường đi
+}Map;
 
-// Khởi tạo Map
+//Khởi tạo Map
 void init_map(Map *map){
-	map->point_list.clear();
-	map->h_list.clear();
-	map->edge_list.clear();
+    map->point_list.clear();
+    map->h_list.clear();
+    map->edge_list.clear();
 }
 
-// Đọc dữ liệu từ file cấu hình
+//Đọc dữ liệu từ file cấu hình
 // - Dòng đầu tiên chưa n và m (n - số đỉnh, m - số cung)
 // - n dòng tiếp theo: tên các đỉnh (kiểu char) và heuristic của đỉnh đó
 // - m dòng tiếp theo: danh sách cung với trọng số (g)
 // - dòng cuối cùng chứa 2 điểm: start và goal - cho biết điểm bắt đầu và kết thúc
-void readFileTxt(string filename, Map *map, char *start, char *goal){ 
-	freopen(filename.c_str(), "r", stdin);
+void readFileTxt(string filename, Map *map, char *start, char *goal){
+    freopen(filename.c_str(),"r", stdin);
 
-	// Đọc số điểm và số đường đi
-	int n, m;
-	scanf("%d %d\n", &n, &m);
+    //Đọc số điểm và số đường đi
+    int n, m;
+    scanf("%d %d\n", &n, &m);
 
-	// Đọc n điểm với heuristic
-	char node;
-	int h;
-	for(int i = 0; i < n; i++){
-		scanf("%c %d\n", &node, &h);
-		map->point_list.push_back(node);
-		map->h_list.push_back(h);
-	}
+    //Đọc n điểm với Heuristic
+    char node;
+    int h;
+    for(int i = 0; i < n; i++){
+        scanf("%c %d\n", &node, &h);
+        map->point_list.push_back(node);
+        map->h_list.push_back(h);
+    }
 
-	// Đọc m đường đi
-	char u, v;
-	int g;
-	for(int i = 0; i < m; i++){
-		scanf("%c %c %d\n", &u, &v, &g);
-		Edge edge = {u, v, g};
-		map->edge_list.push_back(edge);
-	}
+    //Đọc m đường đi
+    char u, v;
+    int g;
+    for(int i = 0; i < m; i++){
+        scanf("%c %c %d\n", &u, &v, &g);
+        Edge edge = {u, v, g};
+        map->edge_list.push_back(edge);
+    }
 
-	// Đọc 2 điểm start và goal
-	scanf("%c %c", start, goal);
+    //Đọc 2 điểm start & goal
+    scanf("%c %c", start, goal);
+
 }
 
-// Khai báo kiểu cấu trúc Node - lưu trữ trạng thái 
-// - Điểm
-// - Node cha
-// - hueristic và chi phí hiện tại
-typedef struct Node {
-	char point;
-	struct Node* parent;
-	int h, g;
+//Khai báo kiểu cấu trúc Node - lưu trữ trạng thái 
+typedef struct Node{
+    char point; //Điểm
+    struct Node* parent; // Node parent
+    int h,g; // heuristic & chi phí hiện tại
 } Node;
 
-// Lấy chỉ số heuristic của một điểm
+//Lấy chỉ số heuristic của một điểm
 int get_heuristic(char point, Map map){
-	for(size_t i = 0; i < map.point_list.size(); i++){
-		if(point == map.point_list.at(i))
-			return map.h_list.at(i);
-	}
-	return -1;
+    for(size_t i = 0; i < map.point_list.size(); i++){
+        if(point == map.point_list.at(i))
+            return map.h_list.at(i);
+    }
+    return -1;
 }
 
 // Lấy chi phí của một đường đi
 int get_cost(int ut, int vt, Map map){
-	for(size_t i = 0; i < map.edge_list.size(); i++){
-		int u = map.edge_list.at(i).u;
-		int v = map.edge_list.at(i).v;
-		if(ut == u && vt == v || ut == v && vt == u)
-			return map.edge_list.at(i).g;
-	}
-	return -1;
+    for(size_t i = 0; i < map.edge_list.size(); i++){
+        int u = map.edge_list.at(i).u;
+        int v = map.edge_list.at(i).v;
+        if(ut == u && vt == v || ut == v && vt == u)
+            return map.edge_list.at(i).g;
+    }
+    return -1;
 }
-
-// Lấy các điểm láng giềng - có đường đi tới điểm đang xét
+// Lấy các điểm láng giềng (Liền kề với điểm đang xét)
 vector<char> get_neighbors(char point, Map map){
-	vector<char> neighbors;
-	for(size_t i = 0; i < map.edge_list.size(); i++){
-		char u = map.edge_list.at(i).u;
-		char v = map.edge_list.at(i).v;
-		if(point == u) neighbors.push_back(v);
-		else if(point == v) neighbors.push_back(u);
-	}
-	return neighbors;
+    vector<char> neighbors;
+    for(size_t i = 0; i < map.edge_list.size(); i++){
+        char u = map.edge_list.at(i).u;
+        char v = map.edge_list.at(i).v;
+        if(point == u) neighbors.push_back(v);
+        else if (point == v) neighbors.push_back(u);
+    }
+    return neighbors;
 }
 
-// Tìm một node chứa điểm đang xét trong một danh sách node - có tham số để lưu vị trí tìm thấy
+//Tìm một node chứa điểm đang xét trong một danh sách các node, lưu lại vị trí tìm thấy
 Node* find_point_in_list(char point, vector<Node*> list, vector<Node*>::iterator *pos){
-	if(list.size() == 0) return NULL;
+    if(list.size() == 0) return NULL;
 
-	vector<Node*>::iterator it = list.begin();
-	while(it != list.end()){
-		if((*it)->point == point){
-			*pos = it;
-			return *it;
-		}
-
-		it = list.erase(it);
-	}
-	return NULL;
+    vector<Node*>::iterator it = list.begin();
+    while(it != list.end()){
+        if((*it)->point == point){
+            *pos = it;
+            return *it;
+        }
+        it = list.erase(it);
+    }
+    return NULL;
 }
 
-// Hàm so sánh -> nhằm mục đích cung cấp cho hàm sort()
-bool compare_h(Node* a, Node* b) { 
-    return a->h > b->h;  
+//Hàm so sánh -> nhằm mục đích cung cấp cho hàm sort
+bool compare_h(Node *a, Node *b){
+    return a->h > b->h;
 }
 
-// Giải thuật Greedy
-Node* GreedyAlgorithm(Map map, char start, char goal, vector<char> *traversing_order) { 
+//Giải thuật Greedy
+Node* GreedyAlgorithm(Map map, char start, char goal, vector<char> *traversing_order){
+    //Khai báo 2 danh sách open & close
+    vector<Node*> openList;
+    vector<Node*> closeList;
 
-	// Khai báo 2 danh sách open và close
-	vector<Node*> openList;
-	vector<Node*> closeList;
+    //Tạo một root node với điểm start
+    Node *root = (Node*)malloc(sizeof(Node));
+    root->point = start;
+    root->parent = NULL;
+    root->h = get_heuristic(start, map);
+    root->g = 0;
 
-	// Tạo một root node với điểm start
- 	Node* root = (Node*)malloc(sizeof(Node));
-	root->point = start;
-	root->parent = NULL;
-	root->h = get_heuristic(start, map);
-	root->g = 0;
+    if(root->h == -1){
+        printf("Heuristic of root is -1");
+        return NULL;
+    }
 
-	if(root->h == -1) {
-		printf("Hueristic of root is -1");
-		return NULL;
-	}
+    //Đưa root node vài open
+    openList.push_back(root);
 
-	// Đưa root node vào open
-	openList.push_back(root);
+    //Lặp khi Open List còn phần tử
+    while(!openList.empty()){
+        //Sắp xếp Openlist theo  giá trị giảm dần của h
+        sort(openList.begin(),openList.end(),compare_h);
 
-	// Lặp khi open list còn phần tử
-	while(!openList.empty()){
-        // Sắp xếp openList chỉ theo h
-        sort(openList.begin(), openList.end(), compare_h);
-		// Lấy node cuối cùng trong open 
-		Node* node = openList.back();
-		// Xóa node này ra khỏi open và đưa vào close
-		openList.pop_back();
-		closeList.push_back(node);
+        //Lấy Node cuối cùng trong open
+        Node* node = openList.back();
 
-		// Thêm node đang xét vào danh sách lưu thứ tự duyệt
-		traversing_order->push_back(node->point);
+        //Xóa node này ra khỏi Open và đưa vào Close
+        openList.pop_back();
+        closeList.push_back(node);
 
-		// Kiểm tra: nếu node đang xét chứa điểm goal -> tìm được lời giải
-		if(node->point == goal) return node;
+        //Thêm node đang xét vào danh sách lưu thứ tự duyệt
+        traversing_order->push_back(node->point);
 
-		// Lấy danh sách và duyệt qua các láng giềng
-		vector<char> neighbors = get_neighbors(node->point, map);
-		for(size_t v = 0; v < neighbors.size(); v++){
+        //Kiểm tra: Nếu node đang xét là goal
+        if(node->point == goal) return node;
 
-			// Tạo node mới với từng láng giềng
-			Node* newNode = (Node*)malloc(sizeof(Node));
-			newNode->point = neighbors.at(v);
-			newNode->parent = node;
-			newNode->h = get_heuristic(newNode->point, map);
-			newNode->g = newNode->parent->g + get_cost(newNode->parent->point, newNode->point, map);
+        //Lấy danh sách các láng giềng của node đang xét
+        vector<char> neighbors = get_neighbors(node->point, map);
+        
+        for(size_t v = 0; v < neighbors.size(); v++){
+            //Tạo node mới với từng láng giềng
+            Node* newNode = (Node*)malloc(sizeof(Node));
+            newNode->point = neighbors.at(v);
+            newNode->parent = node;
+            newNode->h = get_heuristic(newNode->point, map);
+            newNode->g = newNode->parent->g +get_cost(newNode->parent->point, newNode->point, map);
 
-
-			// Kiểm tra sự hiện diện của láng giềng trong close
-			vector<Node*>::iterator pos;
-            if (find_point_in_list(newNode->point, closeList, &pos) != NULL) {
-                free(newNode);
+            //Kiểm tra sự hiện diện của node láng giềng trong close
+            vector<Node*>::iterator pos;
+            if(find_point_in_list(newNode->point,closeList,&pos) != NULL){
+                free(newNode); //Nếu tìm thấy trong close -> Xóa node láng giềng vừa được tìm thấy
                 continue;
             }
 
-            // Kiểm tra sự hiện diện của láng giềng trong open
-            Node* found = find_point_in_list(newNode->point, openList, &pos);
-            if (found == NULL) {
-                openList.push_back(newNode);
+            //Kiểm tra sự hiện diện của node láng giềng trong open
+            Node* found = find_point_in_list(newNode->point,openList,&pos);
+            if(found == NULL){
+                openList.push_back(newNode); //Nếu không tìm thấy trong open -> Thêm nút láng giềng vừa được tìm thấy vào open
             } else {
-                free(newNode);
+                free(newNode); //Nếu tìm thấy trong open -> Xóa node láng giềng vừa được tìm thấy
             }
-		}
-	}
+        }
+    }
 
-	return NULL;
+    return NULL;
 }
 
-// Hiển thị đường đi lời giải
-void print_way_to_goal(Node *node){
-	stack<Node*> path;
+//In đường đi lời giải
+void prin_way_to_goal(Node* node){
+    stack<Node*> path;
 
-	while(node->parent != NULL){
-		path.push(node);
-		node = node->parent;
-	}
-	path.push(node);
+    while(node->parent != NULL){
+        path.push(node);
+        node = node->parent;
+    }
+    path.push(node);
 
-	printf("\nPath to goal: ");
-	while(path.size() > 1){
-		printf("%c -> ", path.top()->point);
-		path.pop();
-	}
-	printf("%c", path.top()->point);
-	path.pop();
+    printf("\nPath to goal: ");
+    while(path.size() > 1){
+        printf("%c -> ", path.top()->point);
+        path.pop();
+    }
+    printf("%c",path.top()->point);
+    path.pop();
 }
 
 int main(){
+    //Khởi tạo map
+    Map map;
+    init_map(&map);
 
-	// Khai báo và khởi tạo Map
-	Map map;
-	init_map(&map);
+    //Đọc dữ liệu từ file cấu hình
+    char start, goal;
+    readFileTxt("map_data.txt", &map, &start, &goal);
 
-	// Đọc dư liệu từ file
-	char start, goal;
-	readFileTxt("map_data.txt", &map, &start, &goal);
+    //Khai báo danh sách chứa thứ tuyệt duyệt
+    vector<char> traversing_order;
 
-	// Khai báo danh sách chứa thứ tự duyệt
-	vector<char> traversing_order;
+    //Triển khai giải thuật
+    Node* node = GreedyAlgorithm(map, start, goal, &traversing_order);
 
-	// Thực hiện giải thuật
-    Node* node = GreedyAlgorithm(map, start, goal, &traversing_order);  
-	
-	// Hiển thị thứ tự duyệt
-	printf("Traversing Order: ");
-	for(size_t i = 0; i < traversing_order.size() - 1; i++){
-		printf("%c -> ", traversing_order.at(i));
-	}
-	printf("%c", traversing_order.back());
+    //In thứ tự duyệt
+    printf("Traversing order: ");
+    for(size_t i = 0; i < traversing_order.size() - 1; i++){
+        printf("%c -> ", traversing_order.at(i));
+    }
+    printf("%c", traversing_order.back());
 
-	// Hiển thị đường đi lời giải
-	print_way_to_goal(node);
+    //In đường đi lời giải
+    prin_way_to_goal(node);
 
-	// Hiển thị tổng chi phí
-	printf("\nTotal cost: %d", node->g);
-	
-	return 0;
+    //In tổng chi phí
+    printf("\nTotal cost: %d", node->g);
+
+    return 0;
 }
